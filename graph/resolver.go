15c09b34bbc778c6b2d8e3184c0f1083be6b2d1f
farisadlin-kargo <faris.adlin@kargo.tech>
2022-05-31 10:41:21 +0700

    Added iteration for query list of trucks

--- graph/resolver.go
+++ graph/resolver.go
@@ -4,10 +4,22 @@ package graph
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 import (
+	"fmt"
+
 	"github.com/farisadlin-kargo/kargo-trucks/graph/model"
 )
 
 type Resolver struct {
 	Trucks    []*model.Truck
 	Shipments []*model.Shipment
+}
+
+func (r *Resolver) Init() {
+	for i := 0; i < 20; i++ {
+		truck := &model.Truck{
+			ID:      fmt.Sprintf("TRUCK-%d", len(r.Trucks)+1),
+			PlateNo: fmt.Sprintf("B %d CD", len(r.Trucks)+1),
+		}
+		r.Trucks = append(r.Trucks, truck)
+	}
 }
